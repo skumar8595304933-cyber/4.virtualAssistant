e75468e6e93f102a41cdf1a1b16039c783115c68
Suraj Kumar <skumar8595304933@gmail.com>
2026-02-13 14:42:26 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,35 @@
+// import express from "express"
+// import dotenv from "dotenv"
+// dotenv.config()
+// import connectDb from "./config/db.js"
+// import authRouter from "./routes/auth.routes.js"
+// import cors from "cors"
+// import cookieParser from "cookie-parser"
+// import userRouter from "./routes/user.routes.js"
+// import geminiResponse from "./gemini.js"
+
+
+// const app=express()
+// app.use(cors({
+//     origin:"https://ai-fronted-uw3h.onrender.com",
+//     credentials:true
+// }))
+
+
+
+// const port=process.env.PORT || 5000
+// app.use(express.json())
+// app.use(cookieParser())
+// app.use("/api/auth",authRouter)
+// app.use("/api/user",userRouter)
+
+
+// app.listen(port,()=>{
+//     connectDb()
+//     console.log("server started")
+// })
+
+////////////////////////--------------------------------------------------------------
 import express from "express"
 import dotenv from "dotenv"
 dotenv.config()
@@ -6,23 +38,25 @@ import authRouter from "./routes/auth.routes.js"
 import cors from "cors"
 import cookieParser from "cookie-parser"
 import userRouter from "./routes/user.routes.js"
-import geminiResponse from "./gemini.js"
 
+const app = express()
 
-const app=express()
+app.set("trust proxy", 1)
+
 app.use(cors({
-    origin:"https://ai-fronted-uw3h.onrender.com",
-    credentials:true
+  origin: "https://ai-fronted-uw3h.onrender.com",
+  credentials: true
 }))
-const port=process.env.PORT || 5000
+
+const port = process.env.PORT || 5000
+
 app.use(express.json())
 app.use(cookieParser())
-app.use("/api/auth",authRouter)
-app.use("/api/user",userRouter)
 
+app.use("/api/auth", authRouter)
+app.use("/api/user", userRouter)
 
-app.listen(port,()=>{
-    connectDb()
-    console.log("server started")
+app.listen(port, () => {
+  connectDb()
+  console.log("server started")
 })
-
