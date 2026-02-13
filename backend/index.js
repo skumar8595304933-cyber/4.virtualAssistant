a620b049cf9facd2dbdee1a586af675037b9fa80
Suraj Kumar <skumar8595304933@gmail.com>
2026-02-13 09:02:13 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -11,7 +11,7 @@ import geminiResponse from "./gemini.js"
 
 const app=express()
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://ai-fronted-uw3h.onrender.com",
     credentials:true
 }))
 const port=process.env.PORT || 5000
